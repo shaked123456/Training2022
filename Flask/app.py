30d4382976c1f7ae5db6ad73e8b620e6293cb36c
shaked123456 <97021896+shaked123456@users.noreply.github.com>
2022-01-03 14:54:52 +0200

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -9,8 +9,4 @@ def home():
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(debug=True, port="2222")
-=======
-    app.run(debug=True, "Intentional mistake)
->>>>>>> c8562df4d42ac3158fad8ac8ab4e3707bb5b5aef
