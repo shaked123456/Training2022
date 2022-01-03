57704d7fd9b3ae4c0b7f6b544f23ca52ac4916e9
shaked123456 <Shaked@wizards.co.il>
2022-01-03 13:49:19 +0200

    first commit

---
+++ Flask/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Test :P"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, "Intentional mistake")
